fc5c0c8698f65a26608c343ab7dc5b2d46ffa2f5
pvijaykrishna549 <you@example.com>
2021-06-11 21:18:06 +0530

    UC-4: Retrive all data from Employee_payroll Database

--- JDBC_PracticeProblem.java
+++ JDBC_PracticeProblem.java
@@ -1,10 +1,7 @@
 package com.bridgelabz.jdbc;
 
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class JDBC_PracticeProblem {
     public static void main(String[] args) {
@@ -19,26 +16,25 @@ public class JDBC_PracticeProblem {
             stmt.setDouble(1,3000000);
             stmt.setInt(2,2);
             stmt.executeUpdate();
-
-
-
-
-
-            // Statement stmt = connection.createStatement();
-           /* ResultSet resultSet = stmt.executeQuery("select * from employee");
+             Statement stmt1 = connection.createStatement();
+           ResultSet resultSet = stmt1.executeQuery("select * from employee");
             while(resultSet.next())
             {
                 System.out.println(resultSet.getInt("Emp_id")+ " " + resultSet.getString("name")+ " " + resultSet.getString("phone_number")
                 +" "+ resultSet.getString("address")+" "+resultSet.getString("gender")+" "+resultSet.getDate("start"));
-            }*/
+            }
+
         }
 
 
         catch (ClassNotFoundException | SQLException e)
         {
-            System.out.println("poijhbv");
+            System.out.println(e);
 
     }
 
     }
 }
+
+
+
